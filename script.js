bb8ca475c644bba22bddce02b3540ab3e77eb22e
DennysMendonca <dennys.charlles.silva.mendonca@alunoedufor.com.br>
2026-03-19 20:54:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,18 @@ form.addEventListener("submit", function(e) {
   mensagem.textContent = "Enviando..."
   mensagem.style.color = "black"
 
+let idade = 42
+
+const dados = {
+  nome,
+  email,
+  idade
+}
+
+console.log(dados)
+console.log(JSON.stringify(dados))
+
+  
   fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     headers: {
